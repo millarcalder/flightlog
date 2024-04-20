9067a3953d675cfcb516618a2c1ee20d0f142dc8
millarcalder <millar9819@gmail.com>
2024-04-20 14:40:17 +0000

    unit tests for auth

--- logbook/backend/logbook/tests/unit/lib/test_auth.py
+++ logbook/backend/logbook/tests/unit/lib/test_auth.py
@@ -1,5 +1,17 @@
-from logbook.lib.auth import TokenData
+from faker import Faker
+from logbook.lib.auth import _generate_password_hash
+from logbook.lib.auth import _verify_password
+
+fake = Faker()
 
 
-def test_token_data():
-    assert TokenData(**{"sub": "abc", "extra": "thing"})
+def test_password_hashing__valid():
+    for password in [fake.word() for _ in range(10)]:
+        hashed_password = _generate_password_hash(password)
+        assert _verify_password(password, hashed_password)
+
+
+def test_password_hashing__invalid():
+    for password in [fake.word() for _ in range(10)]:
+        hashed_password = _generate_password_hash(password)
+        assert not _verify_password('incorrectpassword!', hashed_password)
