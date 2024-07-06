ee849013b743b4e6873844c15199cd00d4419e02
millarcalder <millar9819@gmail.com>
2024-07-06 11:37:37 +0000

    mutations for adding gliders and sites

--- logbook/backend/logbook/graphql/mutations.py
+++ logbook/backend/logbook/graphql/mutations.py
@@ -2,11 +2,11 @@ from datetime import date, datetime
 
 import strawberry
 
-from logbook.db.models import Flight as FlightOrm
+from logbook.db.models import Flight as FlightOrm, Glider as GliderOrm, Site as SiteOrm
 from logbook.graphql import sqlalchemy_to_graphql_model
-from logbook.graphql.queries import Flight
-from logbook.models import Flight as FlightModel
-from logbook.models import FlightInput as FlightInput
+from logbook.graphql.queries import Flight, Glider, Site
+from logbook.models import Flight as FlightModel, Glider as GliderModel, Site as SiteModel
+from logbook.models import FlightInput, GliderInput, SiteInput
 
 
 @strawberry.type
@@ -52,9 +52,76 @@ class FlightMutations:
         # Convert to graphQL model
         return sqlalchemy_to_graphql_model(flight, FlightModel, Flight)
 
+@strawberry.type
+class GliderMutations:
+    @strawberry.mutation
+    def add(
+        self,
+        info: strawberry.Info,
+        user_id: int,
+        model: str,
+        manufacturer: str,
+        rating: str
+    ) -> Glider:
+        # Validate by instantiating pydantic model
+        glider_input = GliderInput(
+            user_id=user_id,
+            model=model,
+            manufacturer=manufacturer,
+            rating=rating
+        )
+
+        # Insert into the database
+        glider = GliderOrm(**glider_input.model_dump())
+        info.context.db_sess.add(glider)
+        info.context.db_sess.commit()
+
+        # Convert to graphQL model
+        return sqlalchemy_to_graphql_model(glider, GliderModel, Glider)
+
+
+@strawberry.type
+class SiteMutations:
+    @strawberry.mutation
+    def add(
+        self,
+        info: strawberry.Info,
+        name: str,
+        description: str,
+        latitude: float,
+        longitude: float,
+        altitude: int,
+        country: str
+    ) -> Site:
+        # Validate by instantiating pydantic model
+        site_input = SiteInput(
+            name=name,
+            description=description,
+            latitude=latitude,
+            longitude=longitude,
+            altitude=altitude,
+            country=country
+        )
+
+        # Insert into the database
+        site = SiteOrm(**site_input.model_dump())
+        info.context.db_sess.add(site)
+        info.context.db_sess.commit()
+
+        # Convert to graphQL model
+        return sqlalchemy_to_graphql_model(site, SiteModel, Site)
+
 
 @strawberry.type
 class Mutation:
     @strawberry.field
     def flight(self) -> FlightMutations:
         return FlightMutations()
+
+    @strawberry.field
+    def glider(self) -> GliderMutations:
+        return GliderMutations()
+
+    @strawberry.field
+    def site(self) -> SiteMutations:
+        return SiteMutations()
--- logbook/backend/logbook/tests/integration/graphql/test_mutations.py
+++ logbook/backend/logbook/tests/integration/graphql/test_mutations.py
@@ -100,3 +100,57 @@ def test_add_flight__invalid_altitude(context):
 
     assert len(result.errors) == 1
     assert "Assertion failed, -10 is not a valid altitude" in result.errors[0].message
+
+
+def test_add_glider(context):
+    mutation = """
+        mutation {
+            glider {
+                add(
+                    userId: 1,
+                    model: "foo",
+                    manufacturer: "bar",
+                    rating: "EN-A"
+                ) {
+                    model
+                    manufacturer
+                    rating
+                }
+            }
+        }
+    """
+    result = schema.execute_sync(mutation, context_value=context)
+    assert result.errors is None
+    assert result.data["glider"]["add"] == {
+        "model": "foo",
+        "manufacturer": "bar",
+        "rating": "EN-A"
+    }
+
+
+def test_add_site(context):
+    mutation = """
+        mutation {
+            site {
+                add(
+                    name: "foo",
+                    description: "...",
+                    latitude: 1.99,
+                    longitude: -1.99,
+                    altitude: 123,
+                    country: "bar"
+                ) {
+                    name
+                    latitude
+                    longitude
+                }
+            }
+        }
+    """
+    result = schema.execute_sync(mutation, context_value=context)
+    assert result.errors is None
+    assert result.data["site"]["add"] == {
+        "name": "foo",
+        "latitude": 1.99,
+        "longitude": -1.99
+    }
